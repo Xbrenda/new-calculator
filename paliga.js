fe20d3b56ec7b36a968a621054363ea057c63cea
Xbrenda <97949164+Xbrenda@users.noreply.github.com>
2023-03-28 12:18:10 +0300

    Add files via upload

---
+++ paliga.js
@@ -0,0 +1,7 @@
+const input1 = document.querySelector('.js-teksts');
+const plusButton = document.querySelector('.js-plus')
+
+
+const Add = (skaitlis1, skaitlis2) => {
+          return skaitlis1+skaitlis2
+}
